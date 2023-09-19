be971d9368eeafe1ee890da111ccaddc13be9d0c
Kaio-code-creator <kaioasousa12@gmail.com>
2023-09-18 22:09:32 -0300

    .

--- Meus Projetos/Pedra papel e tesoura/script.js
+++ Meus Projetos/Pedra papel e tesoura/script.js
@@ -1,50 +1,50 @@
 const choices = ["tesoura", "pedra", "papel"];
-        let playerScore = 0;
-        let computerScore = 0;
+    let playerScore = 0;
+    let computerScore = 0;
 
-        document.getElementById("tesoura").addEventListener("click", () => playRound("tesoura"));
-        document.getElementById("pedra").addEventListener("click", () => playRound("pedra"));
-        document.getElementById("papel").addEventListener("click", () => playRound("papel"));
-        document.getElementById("reset").addEventListener("click", resetGame);
+    document.getElementById("tesoura").addEventListener("click", () => playRound("tesoura"));
+    document.getElementById("pedra").addEventListener("click", () => playRound("pedra"));
+    document.getElementById("papel").addEventListener("click", () => playRound("papel"));
+    document.getElementById("reset").addEventListener("click", resetGame);
 
-        function computerPlay() {
-            const randomIndex = Math.floor(Math.random() * choices.length);
-            return choices[randomIndex];
-        }
+    function computerPlay() {
+        const randomIndex = Math.floor(Math.random() * choices.length);
+        return choices[randomIndex];
+    }
 
-        function playRound(playerSelection) {
-            const computerSelection = computerPlay();
-            let result = "";
+    function playRound(playerSelection) {
+        const computerSelection = computerPlay();
+        let result = "";
 
-            if (playerSelection === computerSelection) {
-                result = "Empate!";
-            } else if (
-                (playerSelection === "tesoura" && computerSelection === "papel") ||
-                (playerSelection === "pedra" && computerSelection === "tesoura") ||
-                (playerSelection === "papel" && computerSelection === "pedra")
-            ) {
-                result = `Você ganhou! ${playerSelection} vence ${computerSelection}.`;
-                playerScore++;
-            } else {
-                result = `Você perdeu! ${computerSelection} vence ${playerSelection}.`;
-                computerScore++;
-            }
+    if (playerSelection === computerSelection) {
+        result = "Empate!";
+    } else if (
+        (playerSelection === "tesoura" && computerSelection === "papel") ||
+        (playerSelection === "pedra" && computerSelection === "tesoura") ||
+        (playerSelection === "papel" && computerSelection === "pedra")
+    ) {
+        result = `Você ganhou! ${playerSelection} vence ${computerSelection}.`;
+        playerScore++;
+    } else {
+        result = `Você perdeu! ${computerSelection} vence ${playerSelection}.`;
+        computerScore++;
+    }
 
-            updateScore();
-            updateResult(result);
-        }
+    updateScore();
+    updateResult(result);
+}
 
-        function updateScore() {
-            document.getElementById("score").textContent = `Pontos: Você ${playerScore} - ${computerScore} Máquina`;
-        }
+function updateScore() {
+    document.getElementById("score").textContent = `Pontos: Você ${playerScore} - ${computerScore} Máquina`;
+}
 
-        function updateResult(resultText) {
-            document.getElementById("result").textContent = resultText;
-        }
+function updateResult(resultText) {
+    document.getElementById("result").textContent = resultText;
+}
 
-        function resetGame() {
-            playerScore = 0;
-            computerScore = 0;
-            updateScore();
-            updateResult("Esperando sua jogada...");
-        }
+function resetGame() {
+    playerScore = 0;
+    computerScore = 0;
+    updateScore();
+    updateResult("Esperando sua jogada...");
+}
